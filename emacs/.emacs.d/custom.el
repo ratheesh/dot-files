6fb9ac246918f8bea8bfd91e7c142b4ba262fa95
Ratheesh S <ratheeshreddy@gmail.com>
2014-01-05 09:58:46 +0530

    emacs: Add custom.el
    Add custom.el with some sane settings.
    
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat)))
+ '(footnote-spaced-footnotes nil)
+ '(sml/theme (quote light))
+ '(visible-cursor t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "white"))))
+ '(font-lock-keyword-face ((t (:foreground "#8ac6f2" :weight normal))))
+ '(font-lock-type-face ((t (:foreground "#92a65e" :weight normal))))
+ '(git-gutter+-separator ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "color-33"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "color-130"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "magenta"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "brightyellow"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "brightblue"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "magenta"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "color-53"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "brightred"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "brightyellow"))))
+ '(sml/col-number ((t nil)))
+ '(sml/filename ((t (:inherit sml/global :foreground "Blue" :weight normal))))
+ '(sml/folder ((t (:inherit sml/global :weight normal))))
+ '(sml/global ((t (:foreground "black" :inverse-video nil))))
+ '(sml/line-number ((t (:inherit sml/modes :weight normal)))))
