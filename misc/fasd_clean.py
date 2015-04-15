c4ffd77a56e09f3b418e6d13e8339fe693fffbdb
Ratheesh S <ratheesh@mistralsolutions.com>
2015-04-15 16:33:18 +0530

    Misc: add fasd_cleanup script
    This script will cleanup obsolete entries in fasd database
    
    Signed-off-by: Ratheesh S <ratheesh@mistralsolutions.com>
    Tested-by: Ratheesh S <ratheesh@mistralsolutions.com>

---
+++ misc/fasd_clean.py
@@ -0,0 +1,55 @@
+#/usr/bin/env python
+# Copyright (C) 2015  Ratheesh S<ratheeshreddy@gmail.com>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+
+import os
+
+db_file = "/home/ratheesh/.fasd"
+purged_items = 0
+
+try:
+    f = open(db_file, "r+")
+except IOError:
+    print 'ERROR: No File found: %s' % db_file
+    exit(1)
+
+d = f.readlines()
+f.close()
+
+try:
+    f = open(db_file, "w+")
+except IOError:
+    print 'ERROR: No File found: %s' % db_file
+    exit(1)
+
+print "Cleaning fasd database ..."
+for i in d:
+    path, sep, misc = i.partition('|')
+    if os.path.exists(path):
+        f.write(i)
+    else:
+        print 'Removing %s' % path
+        purged_items += 1       # increment purged items
+f.close()
+
+if purged_items == 0:
+    print "fasd database is clean!"
+else:
+    print "---------------------------------------"
+    print "No. of Purged Items: %d" % purged_items
+
+# End of File
