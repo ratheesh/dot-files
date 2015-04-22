f0d5644fbe794cfb509348dce4f0c9d4f26659e7
Ratheesh S <ratheesh@mistralsolutions.com>
2015-04-22 10:15:36 +0530

    Emacs: configure guide-key package
    Signed-off-by: Ratheesh S <ratheesh@mistralsolutions.com>
    Tested-by: Ratheesh S <ratheesh@mistralsolutions.com>

--- emacs/.emacs.d/misc-config.el
+++ emacs/.emacs.d/misc-config.el
@@ -1,5 +1,17 @@
 ;;; This file has configuration which are not strictly for developement purpose
 
+;;; keybinding help after a delay
+(use-package guide-key
+  :ensure t
+  :diminish guide-key-mode
+  :init
+  (progn
+    (setq guide-key/guide-key-sequence t))
+  :config
+  (progn
+    (guide-key-mode)))
+
+
 (use-package skype
   :disabled t
   :init
