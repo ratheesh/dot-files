e3b8590eb11265cf9fa0929da139cb793c1ded7b
Ratheesh S <ratheesh@mistralsolutions.com>
2015-04-25 14:49:13 +0530

    Emacs: custom variables change for sml
    Signed-off-by: Ratheesh S <ratheesh@mistralsolutions.com>
    Tested-by: Ratheesh S <ratheesh@mistralsolutions.com>

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -13,9 +13,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(git-gutter+-separator ((t (:foreground "yellow" :weight normal))))
+ '(sml/projectile ((t (:inherit sml/git :slant italic))))
+ '(sml/filename ((t (:inherit sml/global :weight normal))))
+ '(sml/line-number ((t (:inherit sml/modes :weight normal))))
  ;; '(sml/col-number ((t nil)))
- ;; '(sml/filename ((t (:inherit sml/global :foreground "Blue" :weight normal))))
  ;; '(sml/folder ((t (:inherit sml/global :weight normal))))
  ;; '(sml/global ((t (:foreground "black" :inverse-video nil))))
- ;; '(sml/line-number ((t (:inherit sml/modes :weight normal))))
  )
