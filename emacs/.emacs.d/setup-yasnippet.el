1d94916343a03ebab85a5394d4264e7ed983d160
Ratheesh S <ratheesh@mistralsolutions.com>
2015-04-27 09:21:36 +0530

    Emacs: Some yas-mode changes
    Signed-off-by: Ratheesh S <ratheesh@mistralsolutions.com>

--- emacs/.emacs.d/setup-yasnippet.el
+++ emacs/.emacs.d/setup-yasnippet.el
@@ -9,14 +9,35 @@
     (setq yas-melpa-path (expand-file-name "snippets" (file-name-directory (locate-library "yasnippet"))))
     (setq yas-snippet-dirs '())
     (add-to-list 'yas-snippet-dirs (expand-file-name "my-snippets" user-emacs-directory))
-    (add-to-list 'yas-snippet-dirs yas-melpa-path))
+    (add-to-list 'yas-snippet-dirs yas-melpa-path)
+    (setq  yas-wrap-around-region t))
   :config
   (progn
     (yas-global-mode 1)))
 
+;;; dropdown prompt is annoying! So disable that.
 (setq yas-prompt-functions '(yas-ido-prompt
-			     yas-dropdown-prompt
 			     yas-completing-prompt))
+
+;; Inter-field navigation
+(defun yas/goto-end-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas--snippets-at-point)))
+        (position (yas--field-end (yas--snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-end-of-line 1)
+      (goto-char position))))
+
+(defun yas/goto-start-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas--snippets-at-point)))
+        (position (yas--field-start (yas--snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-beginning-of-line 1)
+      (goto-char position))))
+
+(define-key yas-keymap (kbd "C-e") 'yas/goto-end-of-active-field)
+(define-key yas-keymap (kbd "C-a") 'yas/goto-start-of-active-field)
 
 (provide 'setup-yasnippet)
 ;;; End of File
