f1642e93822432f73fbed3bd91a5ca3e775d3ee4
Ratheesh S <ratheesh@mistralsolutions.com>
2015-05-02 08:24:18 +0530

    Emacs: Add save list for desktop pkg
    Signed-off-by: Ratheesh S <ratheesh@mistralsolutions.com>
    Tested-by: Ratheesh S <ratheesh@mistralsolutions.com>

--- emacs/.emacs.d/sane-defaults.el
+++ emacs/.emacs.d/sane-defaults.el
@@ -216,7 +216,35 @@
       desktop-save                t
       desktop-files-not-to-save   "^$" ;reload tramp paths
       desktop-load-locked-desktop nil
-      desktop-restore-frames nil))
+      desktop-restore-frames nil)
+    (setq desktop-globals-to-save
+      (append '((comint-input-ring . 50)
+                (compile-history . 30)
+                desktop-missing-file-warning
+                (dired-regexp-history . 20)
+                (extended-command-history . 50)
+                (face-name-history . 20)
+                (file-name-history . 100)
+                (grep-find-history . 30)
+                (grep-history . 30)
+                (ido-buffer-history . 100)
+                (ido-last-directory-list . 100)
+                (ido-work-directory-list . 100)
+                (ido-work-file-list . 100)
+                (magit-read-rev-history . 50)
+                (minibuffer-history . 50)
+                (org-clock-history . 50)
+                (org-refile-history . 50)
+                (org-tags-history . 50)
+                (query-replace-history . 60)
+                (read-expression-history . 60)
+                (regexp-history . 60)
+                (regexp-search-ring . 20)
+                register-alist
+                (search-ring . 20)
+                (shell-command-history . 50)
+                tags-file-name
+                tags-table-list))))
   :config
   (progn
     (desktop-save-mode t)))
