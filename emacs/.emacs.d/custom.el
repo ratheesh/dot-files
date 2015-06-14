d3df60aa56edc9ee2bbcfa253b4e5f06d49cad16
Ratheesh S <ratheesh@mistralsolutions.com>
2015-06-14 20:04:07 +0530

    Emacs: Customize powerline mode line
    Signed-off-by: Ratheesh S <ratheesh@mistralsolutions.com>
    Tested-by: Ratheesh S <ratheesh@mistralsolutions.com>

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -11,6 +11,7 @@
  '(powerline-default-separator (quote arrow))
  '(powerline-height nil)
  '(powerline-text-scale-factor nil)
+ '(sml/show-remote t)
  '(visible-cursor t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -18,6 +19,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(git-gutter+-separator ((t (:foreground "yellow" :weight normal))))
+ '(powerline-active1 ((t (:inherit mode-line :background "grey22" :foreground "gold"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "grey40" :foreground "deep sky blue"))))
  '(sml/filename ((t (:inherit sml/global :foreground "#eab700" :slant italic :weight normal))))
  '(sml/line-number ((t (:inherit sml/modes :weight normal))))
  '(sml/projectile ((t (:inherit sml/git)))))
