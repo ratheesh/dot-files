1f98f34e0142f249858f0b827273cf9e8f5bf3db
Ratheesh S <ratheesh@mistralsolutions.com>
2015-06-21 21:22:32 +0530

    Emacs: changes to custom theme for parens
    Signed-off-by: Ratheesh S <ratheesh@mistralsolutions.com>
    Tested-by: Ratheesh S <ratheesh@mistralsolutions.com>

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -8,10 +8,9 @@
  '(org-agenda-files
    (quote
     ("~/Dropbox/org/tasks_at_work.org" "~/Dropbox/org/janus.org" "~/Dropbox/org/routines.org")))
- '(powerline-default-separator (quote arrow-fade))
+ '(powerline-default-separator (quote arrow))
  '(powerline-height nil)
  '(powerline-text-scale-factor nil)
- '(sml/mode-width 30)
  '(sml/show-remote t)
  '(visible-cursor t)
  '(which-func-modes t))
@@ -21,10 +20,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(git-gutter+-separator ((t (:foreground "yellow" :weight normal))))
- '(mode-line ((t (:background "black" :foreground "gray60" :box (:line-width 1 :color "dodger blue")))))
+ '(mode-line ((t (:background "black" :foreground "white" :inverse-video nil :box (:line-width 1 :color "dodger blue") :weight light))))
+ '(mode-line-emphasis ((t (:weight light))))
+ '(mode-line-highlight ((t (:box (:line-width 1 :color "grey40")))))
+ '(persp-selected-face ((t (:inherit sml/filename :foreground "lawn green"))))
  '(powerline-active1 ((t (:inherit mode-line :background "grey22" :foreground "gold"))))
  '(powerline-active2 ((t (:inherit mode-line :background "grey40" :foreground "gold1"))))
  '(sml/filename ((t (:inherit sml/global :foreground "#eab700" :slant italic :weight normal))))
  '(sml/line-number ((t (:inherit sml/modes :weight normal))))
  '(sml/projectile ((t (:inherit sml/git :foreground "orange red"))))
- '(which-func ((t (:foreground "OrangeRed1")))))
+ '(which-func ((t (:foreground "DeepSkyBlue1" :slant italic)))))
