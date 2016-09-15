14d8c1fbdc4dd34936f9849e0424b412794283da
Ratheesh S <ratheeshreddy@gmail.com>
2016-09-15 18:23:34 +0530

    spacemacs: init org-page module

--- spacemacs/private/my-orgmode-setup/packages.el
+++ spacemacs/private/my-orgmode-setup/packages.el
@@ -14,6 +14,7 @@
 (defconst my-orgmode-setup-packages
   '(
     org
+    org-page
     ;; org-habit
     ;; remember
     )
@@ -51,5 +52,67 @@
 
 (defun my-orgmode-setup/init-remember ()
   (use-package remember :defer t))
+
+(defun my-orgmode-setup/init-org-page ()
+;;; Org-page setup
+(use-package org-page
+  :ensure t
+  :demand t
+  :init
+  (progn
+    (setq op/repository-directory "/home/ratheesh/ppcport/git/ratheesh.github.io/")
+    (setq
+     op/site-domain "http://ratheesh.github.io/"
+     op/site-main-title "Ratheesh's Tech Ramblings!"
+     op/site-sub-title "==============>"
+     op/personal-github-link "https://github.com/ratheesh"
+     op/personal-disqus-shortname "Ratheesh"
+     op/personal-google-analytics-id "UA-73982336-1")
+
+    (setq op/category-config-alist
+    '(("blog" ;; this is the default configuration
+       :show-meta t
+       :show-comment t
+       :uri-generator op/generate-uri
+       :uri-template "/blog/%y/%m/%d/%t/"
+       :sort-by :date     ;; how to sort the posts
+       :category-index t) ;; generate category index or not
+      ("wiki"
+       :show-meta t
+       :show-comment nil
+       :uri-generator op/generate-uri
+       :uri-template "/wiki/%t/"
+       :sort-by :mod-date
+       :category-index t)
+      ("Kannada"
+       :show-meta t
+       :show-comment nil
+       :uri-generator op/generate-uri
+       :uri-template "/kannada/%t/"
+       :sort-by :mod-date
+       :category-index t)
+      ("Telugu"
+       :show-meta t
+       :show-comment nil
+       :uri-generator op/generate-uri
+       :uri-template "/telugu/%t/"
+       :sort-by :mod-date
+       :category-index t)
+      ("index"
+       :show-meta nil
+       :show-comment nil
+       :uri-generator op/generate-uri
+       :uri-template "/"
+       :sort-by :date
+       :category-index nil)
+      ("about"
+       :show-meta nil
+       :show-comment nil
+       :uri-generator op/generate-uri
+       :uri-template "/about/"
+       :sort-by :date
+       :category-index nil)))))
+
+)
 
 ;;; packages.el ends here
