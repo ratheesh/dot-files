b0ce2ad4f0e6a897142c18e7efd9ac23a9c56829
Ratheesh S <ratheeshreddy@gmail.com>
2016-10-13 12:49:39 +0530

    tmux:flat: Add support for battery status display
    Currently this does not have support for tmux plugin as this is
    implemented as a plain script with no colour support.
    
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>
    Tested-by: Ratheesh S <ratheeshreddy@gmail.com>

---
+++ tmux/.tmux/tmux.battery.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+battery_soc=`acpi|cut -d: -f 2|cut -d, -f 2 | sed -e "s/\s//" -e "s/%.*//"`
+charging_status=`acpi|cut -d: -f 2|cut -d, -f 1 | sed -e "s/\s//"`
+
+# echo "Battery SOC: $battery_soc"
+# echo "Charging Status: $charging_status"
+
+command_exists() {
+    local command="$1"
+    type "$command" >/dev/null 2>&1
+}
+
+# Check if acpi command is present. Else this command does not work!
+if command_exists "acpi"; then
+    if (( $battery_soc > "85" && $battery_soc <= "100" ));then
+        echo -n ''
+    elif (( $battery_soc > "65" && $battery_soc <= "85" ));then
+        echo -n ''
+    elif (( $battery_soc > "35" && $battery_soc <= "65" ));then
+        echo -n ''
+    elif (( $battery_soc > "15" && $battery_soc <= "35" ));then
+        echo -n ''
+    elif (( $battery_soc > "0" && $battery_soc <= "15" ));then
+        echo -n ''
+    else
+        echo -n 'NA'
+    fi
+
+    if [ "$charging_status" = "Full" ];then
+        echo -n ' '
+    elif [ "$charging_status" = "Charging" ];then
+        echo -n ' '
+    else
+        echo -n ''
+    fi
+fi
+# End of File
