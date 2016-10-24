c1fcb7a191c4e2502ffd1b4dbe387ae595ee9d85
Ratheesh S <ratheeshreddy@gmail.com>
2016-10-24 10:57:40 +0530

    tmux: battery color based on current status
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>
    Tested-by: Ratheesh S <ratheeshreddy@gmail.com>

--- tmux/.tmux/tmux.battery.sh
+++ tmux/.tmux/tmux.battery.sh
@@ -14,23 +14,23 @@ command_exists() {
 # Check if acpi command is present. Else this command does not work!
 if command_exists "acpi"; then
     if (( $battery_soc > "85" && $battery_soc <= "100" ));then
-        echo -n ''
+        echo -n '#[bg=colour234,fg=colour14]#[default]'
     elif (( $battery_soc > "65" && $battery_soc <= "85" ));then
-        echo -n ''
+        echo -n '#[bg=colour234,fg=colour142]#[default]'
     elif (( $battery_soc > "35" && $battery_soc <= "65" ));then
-        echo -n ''
+        echo -n '#[bg=colour234,fg=colour3]#[default]'
     elif (( $battery_soc > "15" && $battery_soc <= "35" ));then
-        echo -n ''
+        echo -n '#[bg=colour234,fg=colour1]#[default]'
     elif (( $battery_soc > "0" && $battery_soc <= "15" ));then
-        echo -n ''
+        echo -n '#[bg=colour234,fg=colour1]#[default]'
     else
         echo -n 'NA'
     fi
 
     if [ "$charging_status" = "Full" ];then
-        echo -n ' '
+        echo -n '#[bg=colour234,fg=colour12] #[default]'
     elif [ "$charging_status" = "Charging" ];then
-        echo -n ' '
+        echo -n ' #[bg=colour234,fg=colour11]#[default]'
     else
         echo -n ''
     fi
