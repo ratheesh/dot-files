c6aa13a9ac67727477a64fe453e6ef7f23e3e638
Ratheesh S <ratheeshreddy@gmail.com>
2017-09-18 08:32:34 +0530

    tmux:256colors: add script for net status
    This is to eliminate need of a online status plugin
    
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>
    Tested-by: Ratheesh S <ratheeshreddy@gmail.com>

---
+++ tmux/.tmux/tmux.netstat.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is only compatible with tmux.
+
+nc -z -w 1 8.8.8.8 53 &>/dev/null
+
+if [ $? -ne 0 ];then
+    echo -e "#[fg=colour160,bg=colour234]●#[default]"
+else
+    echo -e "#[fg=colour33,bg=colour234]●#[default]"
+fi
+
+# echo -e "#[fg=colour34,bold]↓#[fg=colour244,nobold]$((RXDIF/1024))#[fg=colour231,italics]K \
+#[fg=colour197,bold,noitalics]↑#[fg=colour244,nobold]$((TXDIF/1024))#[fg=colour231,italics]K#[default]"
