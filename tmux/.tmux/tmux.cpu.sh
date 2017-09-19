18815ca9d419567e75f47e0accfe027b1cb05f7f
Ratheesh S <ratheeshreddy@gmail.com>
2017-09-19 13:52:06 +0530

    tmux:256colors: remove dependency of tmux plugins
    * This commit completly remove dependency of tmux plugins by removing dependency
    on
    - prefix highlighting plugin
    - cpu percentage plugin (add new script)
    * Also remove copy-mode enter upon prefix+esc key combo. This causes issue during
    vim editing session
    
    Tmux plugins are disabled until we need them in the future
    
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>
    Tested-by: Ratheesh S <ratheeshreddy@gmail.com>

---
+++ tmux/.tmux/tmux.cpu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v iostat)" ]; then
+	echo -e 'NA'
+	exit 1
+else
+	cpu=$(iostat -c 1 2 | tail -n 2 | head -n 1 | awk '{usage=100-$NF} END {printf("%.1f%%", usage)}' | sed 's/,/./')
+	echo -e ${cpu}
+fi
+# End of File
