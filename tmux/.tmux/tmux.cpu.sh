d1cfa2a8cf933f59ce1ed0f71c61075a74c01ac3
Ratheesh S <ratheeshreddy@gmail.com>
2017-10-02 21:57:45 +0530

    tmux:script: change command to calculate cpu usage
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>
    Tested-by: Ratheesh S <ratheeshreddy@gmail.com>

--- tmux/.tmux/tmux.cpu.sh
+++ tmux/.tmux/tmux.cpu.sh
@@ -4,7 +4,8 @@ if ! [ -x "$(command -v iostat)" ]; then
 	echo -e 'NA'
 	exit 1
 else
-	cpu=$(iostat -c 1 2 | tail -n 2 | head -n 1 | awk '{usage=100-$NF} END {printf("%.1f%%", usage)}' | sed 's/,/./')
-	echo -e ${cpu}
+	cpu=$(ps -A -o pcpu | tail -n+2 | paste -sd+ | bc)
+	# cpu=$(iostat -c 1 2 | tail -n 2 | head -n 1 | awk '{usage=100-$NF} END {printf("%.1f%%", usage)}' | sed 's/,/./')
+	echo -e ${cpu}%
 fi
 # End of File
