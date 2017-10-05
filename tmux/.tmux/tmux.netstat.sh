fbdb710c51dc40a9c7009949115e652b99db7987
Ratheesh S <ratheeshreddy@gmail.com>
2017-10-05 10:20:43 +0530

    tmux:scripts:netstat: change icon for netstat
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>
    Tested-by: Ratheesh S <ratheeshreddy@gmail.com>

--- tmux/.tmux/tmux.netstat.sh
+++ tmux/.tmux/tmux.netstat.sh
@@ -5,9 +5,9 @@
 nc -z -w 1 8.8.8.8 53 &>/dev/null
 
 if [ $? -ne 0 ];then
-    echo -e "#[fg=colour160,bg=colour234]●#[default]"
+    echo -e "#[fg=colour160,bg=colour234]⏺#[default]"
 else
-    echo -e "#[fg=colour33,bg=colour234]●#[default]"
+    echo -e "#[fg=colour33,bg=colour234]⏺#[default]"
 fi
 
 # echo -e "#[fg=colour34,bold]↓#[fg=colour244,nobold]$((RXDIF/1024))#[fg=colour231,italics]K \
