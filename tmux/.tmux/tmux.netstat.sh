9d03298746259ce10d305d8fc0b00726f123c36f
Ratheesh S <ratheeshreddy@gmail.com>
2018-06-29 09:41:39 +0530

    tmux:scripts:netstat: remove bg color
    Remove bg color of internet indicator glyph to match default statusbar
    bg color.
    
    Reviewed-by: Ratheesh S <ratheeshreddy@gmail.com>

--- tmux/.tmux/tmux.netstat.sh
+++ tmux/.tmux/tmux.netstat.sh
@@ -3,12 +3,10 @@
 # This script is only compatible with tmux.
 
 nc -z -w 1 8.8.8.8 53 &>/dev/null
-
 if [ $? -ne 0 ];then
-    echo -e "#[fg=colour160,bg=colour234]⏺#[default]"
+    echo -e "#[fg=colour160]⏺#[default]"
 else
-    echo -e "#[fg=colour33,bg=colour234]⏺#[default]"
+    echo -e "#[fg=colour33]⏺#[default]"
 fi
 
-# echo -e "#[fg=colour34,bold]↓#[fg=colour244,nobold]$((RXDIF/1024))#[fg=colour231,italics]K \
-#[fg=colour197,bold,noitalics]↑#[fg=colour244,nobold]$((TXDIF/1024))#[fg=colour231,italics]K#[default]"
+# End of File
