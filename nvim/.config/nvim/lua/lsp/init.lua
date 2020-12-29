f791e28f7669b17f6815cb0fe35f023569575e35
Ratheesh S <ratheeshreddy@gmail.com>
2020-12-29 18:18:16 +0530

    vim: Move lspconfig to standard lua file
    Signed-off-by: Ratheesh S <ratheeshreddy@gmail.com>

---
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -0,0 +1,62 @@
+-- Copyright (C) 2020  Ratheesh <ratheeshreddy@gmail.com>
+-- Author: Ratheesh S
+--
+-- Configuration of NeoViM built-in LSP client
+-- Works only on NeoVim v.0.5.0 and newer versions
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+local lspconfig = require'lspconfig'
+local diagnostic = require'diagnostic'
+local completion = require'completion'
+
+vim.lsp.callbacks["textDocument/publishDiagnostics"] = function() end
+
+local on_attach = function(_, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings
+  local opts = { noremap=true, silent=true  }
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+end
+
+local servers = {'pyls', 'bashls', 'vimls', 'cmake'}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+  }
+end
+
+lspconfig.gopls.setup{
+    root_dir = lspconfig.util.root_pattern('.git');
+}
+
+lspconfig.clangd.setup {
+  cmd = { "clangd-10", "--background-index", "--pch-storage=memory",
+  "--clang-tidy", "--suggest-missing-includes", "--header-insertion=iwyu",
+  "--header-insertion-decorators"};
+}
+
+vim.api.nvim_command('echomsg "Configured NeoViM LSP Client!"')
+
+-- End of File
+
