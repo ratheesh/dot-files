23c5c37fba24fca82a7e2d376d03ad71f4930ff8
Ratheesh S <ratheeshreddy@gmail.com>
2021-02-14 10:05:22 +0530

    vim: re-enable ncm2 instead of nvim-compe
    There is lot of improvement still needed for compe. Use ncm2 for now.
    The issue of not completing structure fields are solved when ncm2 lsp
    client is used instead of lsp. This is good for now.

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -26,9 +26,10 @@
 -- bashls
 -- $ npm install -g bashls
 
-local lspconfig = require'lspconfig'
+local lspconfig  = require'lspconfig'
 local diagnostic = require'diagnostic'
-local compe = require'compe'
+local ncm2       = require('ncm2')
+-- local compe = require'compe'
 
 -- ViM specific settings
 vim.o.completeopt = "noinsert,menuone,noselect"
@@ -36,8 +37,51 @@ vim.o.pumheight = 10
 
 vim.lsp.callbacks["textDocument/publishDiagnostics"] = nil
 
+--- define an chain complete list
+local chain_complete_list = {
+  default = {
+    {complete_items = {'snippet'}},
+    {complete_items = {'path'}, triggered_only = {'/'}},
+    {complete_items = {'lsp'}},
+    {complete_items = {'buffer'}},
+    {complete_items = {'buffers'}},
+    {complete_items = {'tmux'}},
+  },
+
+  string = {
+    {complete_items  = {'path'}, triggered_only   = {'/'}},
+    { complete_items = { 'buffer', 'buffers'   } },
+  },
+
+  comment = {
+    { complete_items = { 'path'   } },
+    { complete_items = { 'buffer' } },
+    { complete_items = {'buffers'} }
+  },
+}
+
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- passing in a table with on_attach function
+  -- require'completion'.on_attach({
+  --   enable_snippet         = 'vsnippet',
+  --   enable_auto_popup      = 1,
+  --   enable_auto_signature  = 1,
+  --   auto_change_source     = 1,
+  --   enable_auto_hover      = 1,
+  --   enable_auto_signature  = 1,
+  --   enable_auto_paren      = 1,
+  --   matching_smart_case    = 1,
+  --   trigger_on_delete      = 1,
+  --   auto_change_source     = 1,
+  --   trigger_keyword_length = 1,
+  --   timer_cycle            = 200,
+  --   confirm_key            = "<C-y>",
+  --   sorting                = 'alphabet',
+  --   matching_strategy_list = {'exact', 'substring', 'fuzzy', 'all'},
+  --   chain_complete_list    = chain_complete_list,
+  -- })
 
   -- Mappings
   local opts = { noremap=true, silent=true  }
@@ -51,22 +95,24 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 end
 
-local servers = {'clangd','gopls','jedi_language_server','bashls', 'vimls'}
+local servers = {'ccls','gopls','jedi_language_server','bashls', 'vimls'}
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
+    on_init = ncm2.register_lsp_source,
   }
 end
 
 lspconfig.clangd.setup {
   on_attach = on_attach,
+  on_init = ncm2.register_lsp_source,
   cmd = {
     '/bin/clangd', '--background-index', '--header-insertion=iwyu', '--suggest-missing-includes', '--cross-file-rename'
   },
 
   init_options = {
     clangdFileStatus = true,
-    usePlaceholders = true,
+    usePlaceholders = false,
     completeUnimported = true,
     semanticHighlighting = true
   }
@@ -75,6 +121,7 @@ lspconfig.clangd.setup {
 -- placeholder option will only work in recent (after 7-Oct-2019)
 lspconfig.ccls.setup {
   on_attach = on_attach,
+  on_init = ncm2.register_lsp_source,
   init_options = {
     cache = {
       directory    = "/home/ratheesh/.ccls-cache";
@@ -92,7 +139,7 @@ lspconfig.ccls.setup {
       snippetSupport = true;
     };
     completion = {
-      placeholder   = false;
+      placeholder   = true;
       detailedLabel = false;
       spellChecking = true;
       -- filterAndSort = false;
