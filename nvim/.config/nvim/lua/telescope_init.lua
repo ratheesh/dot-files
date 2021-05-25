4ecbdab188d0bf3a9452f98241ac3ca3c109b531
Ratheesh S <ratheeshreddy@gmail.com>
2021-05-25 13:17:11 +0530

    vim:telescope: set winblend to value 10

--- nvim/.config/nvim/lua/telescope_init.lua
+++ nvim/.config/nvim/lua/telescope_init.lua
@@ -63,7 +63,7 @@ require('telescope').setup {
     file_ignore_patterns = {},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
     shorten_path = true,
-    winblend = 0,
+    winblend = 10,
     width = 0.75,
     preview_cutoff = 120,
     results_height = 1,
