6318ed0564c11ec4334d1beb2033e0728dc66cab
Ratheesh S <ratheeshreddy@gmail.com>
2021-06-14 17:38:33 +0530

    nvim:windline: re-enable with few refinements
    - Add paste mode component
    - Add readonly glyph in case file is readonly.
    - Add custom function for lsp client

--- nvim/.config/nvim/lua/windline_init.lua
+++ nvim/.config/nvim/lua/windline_init.lua
@@ -34,6 +34,10 @@ local hl_list = {
   GitDiffAdded   = { 'GitAddedFg', 'ActiveBg' },
   GitDiffRemoved = { 'GitRemovedFg', 'ActiveBg' },
   GitDiffChanged = { 'GitChangedFg', 'ActiveBg' },
+
+  -- spell right sep
+  PasteRightProjSep = { 'PasteModeBg', 'ProjectNameBg' },
+  PasteRightFileSep = { 'PasteModeBg', 'FileNameBg' },
 }
 
 local hide_in_width = function() return vim.fn.winwidth(0) > 90 end
@@ -93,6 +97,11 @@ basic.mode_rightsep = {
     FileVisualAfter     = { 'FileNameBg', 'ModeVisualBg' },
     FileReplaceAfter    = { 'FileNameBg', 'ModeReplaceBg' },
     FileCommandAfter    = { 'FileNameBg', 'ModeCommandBg' },
+    PasteNormalAfter    = { 'PasteModeBg', 'ModeNormalBg' },
+    PasteInsertAfter    = { 'PasteModeBg', 'ModeInsertBg' },
+    PasteVisualAfter    = { 'PasteModeBg', 'ModeVisualBg' },
+    PasteReplaceAfter   = { 'PasteModeBg', 'ModeReplaceBg' },
+    PasteCommandAfter   = { 'PasteModeBg', 'ModeCommandBg' },
     ProjectNormalAfter  = { 'ProjectNameBg', 'ModeNormalBg' },
     ProjectInsertAfter  = { 'ProjectNameBg', 'ModeInsertBg' },
     ProjectVisualAfter  = { 'ProjectNameBg', 'ModeVisualBg' },
@@ -100,6 +109,11 @@ basic.mode_rightsep = {
     ProjectCommandAfter = { 'ProjectNameBg', 'ModeCommandBg' },
   },
   text = function()
+  if vim.o.paste then
+    return {
+      { sep.left_rounded, 'Paste' .. state.mode[2] .. 'After' }
+    }
+  end
   if git_comps.is_git() then
     return {
       { sep.left_rounded, 'Project' .. state.mode[2] .. 'After' },
@@ -110,6 +124,32 @@ basic.mode_rightsep = {
     }
   end
 end,
+}
+
+basic.paste_mode = {
+  hl_colors = {
+    default             = { 'FileNameFg', 'FileNameBg'},
+    sep_right_proj      = hl_list.PasteRightProjSep,
+    sep_right_file      = hl_list.PasteRightFileSep,
+    paste_mode          = { 'PasteModeFg', 'PasteModeBg'},
+    FileNormalAfter     = { 'FileNameBg', 'ModeNormalBg' },
+    FileInsertAfter     = { 'FileNameBg', 'ModeInsertBg' },
+  },
+  text = function()
+    if vim.o.paste then
+      if git_comps.is_git() then
+	return {
+	  { 'ραstɛ', 'paste_mode', },
+	  { sep.right_rounded..' ', 'sep_right_proj' },
+	}
+      else
+	return {
+	  { 'ραstɛ', 'paste_mode', },
+	  { sep.right_rounded..' ', 'sep_right_file' },
+	}
+      end
+    end
+  end,
 }
 
 local function project_name()
@@ -129,10 +169,8 @@ basic.projectname = {
 
   },
   text = function()
-    if is_project_valid() then
+    if git_comps.is_git() then
       return {
-        -- { ' ', 'project' },
-        -- { vim.call('utils#getprojectname'), 'project' },
         { project_name(), 'project' },
       }
     end
@@ -151,11 +189,17 @@ basic.file_leftsep = {
       { ' ', 'default' },
     }
     else
-      -- return { { ' ', 'default' } }
       return ''
   end
 end,
 }
+
+local function is_file_ro()
+  if vim.bo.readonly then
+    return ''
+  end
+  return ''
+end
 
 basic.file = {
   name = 'file',
@@ -163,6 +207,7 @@ basic.file = {
     default      = hl_list.File,
     FileName     = {'FileNameFg', 'FileNameBg'},
     FileModified = {'FileNameModFg', 'FileNameBg'},
+    FileRO       = {'FileNameROFg', 'FileNameBg'},
     FileIcon     = hl_list.FileIcon
   },
   text = function()
@@ -170,7 +215,8 @@ basic.file = {
       { b_components.file_icon(''), 'FileIcon' },
       { ' ', 'FileName' },
       { b_components.file_name(''), 'FileName' },
-      { b_components.file_modified('✱ '), 'FileModified' },
+      { b_components.file_modified(' '), 'FileModified' },
+      { is_file_ro(), 'FileRO' },
     }
   end,
 }
@@ -197,7 +243,8 @@ basic.git = {
 
 -- Right side segment
 
-local check_lsp_status = lsp_comps.check_lsp({})
+---------------------------------------------
+-- local check_lsp_status = lsp_comps.check_lsp({})
 basic.lsp_diagnos = {
   name = 'diagnostic',
   hl_colors = {
@@ -206,28 +253,64 @@ basic.lsp_diagnos = {
     blue   = { 'blue', 'ActiveBg' },
   },
   text = function()
-    if check_lsp_status() then
+    if lsp_comps.check_lsp() then
       return {
-        { lsp_comps.lsp_error({ format = '  %s', show_zero = false }), 'red' },
-        { lsp_comps.lsp_warning({ format = '  %s', show_zero = false}), 'yellow' },
         { lsp_comps.lsp_hint({ format = '  %s', show_zero = false }), 'blue' },
+        { lsp_comps.lsp_warning({ format = '  %s', show_zero = false}), 'yellow' },
+        { lsp_comps.lsp_error({ format = '  %s', show_zero = false }), 'red' },
       }
     end
     return ''
   end,
 }
 
+-- local get_lsp_client = function (msg)
+--   msg = msg or 'No Active Lsp'
+--   local buf_ft = vim.api.nvim_buf_get_option(0,'filetype')
+--   local clients = vim.lsp.get_active_clients()
+--   if next(clients) == nil then
+--     return msg
+--   end
+
+--   for _,client in ipairs(clients) do
+--     local filetypes = client.config.filetypes
+--     if filetypes and vim.fn.index(filetypes,buf_ft) ~= -1 then
+--       return client.name
+--     end
+--   end
+--   return msg
+-- end
+
+local function lsp_client_names(component)
+    local clients = {}
+    local icon = component.icon or ' '
+
+    for _, client in pairs(vim.lsp.buf_get_clients()) do
+        clients[#clients+1] = icon .. client.name
+        -- clients[#clients+1] = client.name
+    end
+
+    return table.concat(clients, ' ')
+end
 -- local lsp_server_name = lsp_comps.lsp_name()
-basic.lspclient = {
+basic.lsp_client = {
   hl_colors = {
-    sep_before = { 'IndentBg', 'ActiveBg' },
-    lsp_cl     = { 'LSPClientFg', 'LSPClientBg' },
+    sep_before       = { 'LSPClientBg', 'NormalBg' },
+    sep_before_empty = { 'FileInfoBg', 'NormalBg' },
+    lsp_cl           = { 'LSPClientFg', 'LSPClientBg' },
+    sep_after        = { 'FileInfoBg', 'LSPClientBg' },
   },
   text = function()
-    if check_lsp_status() then
+    if lsp_comps.check_lsp() then
       return {
         { sep.left_rounded, 'sep_before' },
-        { lsp_comps.lsp_name('L'), 'lsp_cl' },
+        { lsp_client_names({}), 'lsp_cl' },
+        { ' ', ''},
+        { sep.left_rounded, 'sep_after' },
+      }
+    else
+      return {
+        { sep.left_rounded, 'sep_before_empty'}
       }
     end
   end,
@@ -241,7 +324,7 @@ basic.fileinfo = {
   },
   text = function()
       return {
-        { sep.left_rounded, 'sep_before' },
+        -- { sep.left_rounded, 'sep_before' },
         { b_components.file_icon(), 'file_type' },
         { ' ', '' },
         { b_components.file_type(), 'file_type' },
@@ -255,7 +338,7 @@ basic.fileinfo = {
 basic.indent = {
   hl_colors = {
     sep_before = { 'IndentBg', 'FileInfoBg' },
-    color      = { 'black', 'IndentBg' },
+    color      = { 'IndentFg', 'IndentBg' },
   },
   text = function()
     local sw = vim.bo.shiftwidth
@@ -268,28 +351,28 @@ basic.indent = {
     end
     return {
       { sep.left_rounded, 'sep_before' },
-      {string.format('%s%s≡ ', sw, im), 'color'}
+      {string.format('%s%s≡', sw, im), 'color'}
     }
   end,
 }
 
 basic.right = {
   hl_colors = {
-    sep_before = { 'RightBg', 'IndentBg' },
+    sep_before = { 'IndentBg', 'RightBg' },
     sep_after  = { 'RightBg', 'black' },
-    text       = { 'white', 'RightBg' },
+    text       = { 'black', 'RightBg' },
     lineno     = { 'LineNoFg', 'LineNoBg' },
     sep        = { 'black', 'RightBg', 'bold' },
   },
   text = function()
     return {
-      { sep.left_rounded, 'sep_before' },
-      { 'ch:%02Bh', 'text' },
+      { sep.right_rounded, 'sep_before' },
+      { ' ch:%02Bh', 'text' },
       {'│','sep'},
       { '', 'text' },
       {' %l:%v','text'},
       {'│','sep'},
-      {'%2p%%','text'},
+      {'%p%%','text'},
       { sep.right_rounded, 'sep_after' },
     }
   end,
@@ -301,6 +384,7 @@ local default = {
     { ' ', hl_list.Black },
     basic.vi_mode,
     basic.mode_rightsep,
+    basic.paste_mode,
     basic.projectname,
     basic.file_leftsep,
     basic.file,
@@ -310,7 +394,7 @@ local default = {
     basic.divider,
     basic.lsp_diagnos,
     { ' ', hl_list.Active },
-    -- basic.lspclient,
+    basic.lsp_client,
     basic.fileinfo,
     basic.indent,
     basic.right,
@@ -330,8 +414,8 @@ local default = {
 windline.setup({
   colors_name = function(colors)
     -- Common colors
-    colors.black_light   = "#595B83"
-    colors.green_light   = "#99c794"
+    -- colors.black_light   = "#595B83"
+    -- colors.green_light   = "#99c794"
     colors.NormalFg      = "#c5cdd9"
     colors.NormalBg      = "#383a4c"
     colors.ActiveFg      = "#c5cdd9"
@@ -340,40 +424,45 @@ windline.setup({
     colors.InActiveBg    = "#383a4c"
 
     -- Mode
-    colors.ModeNormalBg  = "#c5cdd9"
+    colors.ModeNormalBg  = "#B0B7CF"
     colors.ModeInsertBg  = "#00A59E"
     colors.ModeVisualBg  = "#F7CAB8"
     colors.ModeReplaceBg = "#D85A93"
     colors.ModeCommandBg = "#AE8A7E"
 
     -- termguicolors
-    colors.FileNameFg    = "#fefefe"
+    colors.FileNameFg    = "#FEFEFE"
     colors.FileNameBg    = "#595B83"
     colors.FileNameModFg = "#00AFDB"
+    colors.FileNameROFg  = "#EC5F67"
 
-    colors.GitAddedFg    = "#99c794"
+    colors.PasteModeFg   = "#eeeeee"
+    colors.PasteModeBg   = "#AC547E"
+
+    colors.GitAddedFg    = "#4eb899"
     colors.GitChangedFg  = "#B99AB9"
-    colors.GitRemovedFg  = "#ec5f67"
+    colors.GitRemovedFg  = "#EA4050"
 
-    colors.LSPDiagErrFg  = "#FF3140"
-    colors.LSPDiagWarnFg = "#d3869b"
-    colors.LSPDiagHintFg = "#99c794"
+    colors.LSPDiagErrFg  = "#FD5866"
+    colors.LSPDiagWarnFg = "#729FCF"
+    colors.LSPDiagHintFg = "#9987A4"
 
-    colors.LSPClientFg   = "#000000"
-    colors.LSPClientBg   = "#d3869b"
+    colors.LSPClientFg   = "#eeeeee"
+    colors.LSPClientBg   = "#595B83"
 
-    colors.ProjectNameFg = "#eeeeee"
-    colors.ProjectNameBg = "#ac547e"
+    colors.ProjectNameFg = "#EEEEEE"
+    colors.ProjectNameBg = "#2087CF"
 
     colors.FileInfoFg    = "#000000"
-    colors.FileInfoBg    = "#d3869b"
+    colors.FileInfoBg    = "#D3869B"
 
-    colors.IndentBg      = "#B3C1A9"
-    colors.SearchCntFg   = "#4eb899"
+    colors.IndentFg      = "#eeeeee"
+    colors.IndentBg      = "#605FAB"
+    colors.SearchCntFg   = "#4EB899"
 
     colors.LineNoFg      = "#eeeeee"
-    colors.LineNoBg      = "#335078"
-    colors.RightBg       = "#025660"
+    colors.LineNoBg      = "#A070C8"
+    colors.RightBg       = "#B3C1A9"
 
     return colors
   end,
