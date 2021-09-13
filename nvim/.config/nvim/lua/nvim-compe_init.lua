7a3e8f3c5b7432acbe2fc09947a9950a21cfc365
Ratheesh S <ratheeshreddy@gmail.com>
2021-09-13 15:14:57 +0530

    nvim:compe: assorted changes

--- nvim/.config/nvim/lua/nvim-compe_init.lua
+++ nvim/.config/nvim/lua/nvim-compe_init.lua
@@ -28,10 +28,20 @@ require'compe'.setup {
   throttle_time        = 80;
   source_timeout       = 200;
   incomplete_delay     = 400;
-  max_abbr_width       = 100;
-  max_kind_width       = 100;
-  max_menu_width       = 100;
-  documentation        = true;
+  max_abbr_width       = 80;
+  max_kind_width       = 80;
+  max_menu_width       = 80;
+  -- documentation        = true;
+
+  documentation = {
+    border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+    winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+    max_width = 120,
+    min_width = 60,
+    max_height = math.floor(vim.o.lines * 0.3),
+    min_height = 1,
+  };
+
 
   source = {
     ultisnips     = {priority = 50};
@@ -42,10 +52,11 @@ require'compe'.setup {
     spell         = {priority = 45};
     calc          = {priority = 44};
     nvim_lua      = {priority = 43};
-    treesitter    = true;
+    treesitter    = false;
     vsnip         = false;
     tmux          = true;
     snippets_nvim = false;
+    orgmode       =  true;
   };
 }
 
