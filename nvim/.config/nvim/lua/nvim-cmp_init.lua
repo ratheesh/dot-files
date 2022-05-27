790d0be3f048122d4433711db0ea1328b9b75c1d
Ratheesh S <ratheeshreddy@gmail.com>
2022-05-27 11:51:37 +0530

    nvim:cmp: Adjust changes to the new config

--- nvim/.config/nvim/lua/nvim-cmp_init.lua
+++ nvim/.config/nvim/lua/nvim-cmp_init.lua
@@ -40,151 +40,156 @@ local check_back_space = function()
 end
 
 cmp.setup({
-    snippet = {
+  snippet = {
     expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
     -- expand = function(args) vim.fn["UltiSnips#Anon"](args.body) end,
     -- expand = function(args) require('luasnip').lsp_expand(args.body) end,
-    },
+  },
+  window = {
     completion = {
-        completeopt = "menu,menuone,noselect",
-        keyword_length = 2,
-        border = 'rounded',
-        -- completeopt = "menu,menuone,noinsert",
-        get_trigger_characters = function(trigger_characters)
-            return vim.tbl_filter(function(char)
-                return char ~= ' '
-            end, trigger_characters)
-        end,
-      -- autocomplete = {
-      --   types.cmp.TriggerEvent.TextChanged,
-      -- },
-        autocomplete = {
-          cmp.TriggerEvent.InsertEnter,
-          cmp.TriggerEvent.TextChanged
-        }
-    },
-    formatting = {
-        fields = { 'abbr', 'kind', 'menu' },
-        format = function(entry, vim_item)
-            vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
-             -- set a name for each source
-            vim_item.menu = ({
-                vsnip         = "[VSnip]",
-                spell         = "[Spell]",
-                buffer        = "[Buffer]",
-                calc          = "[Calc]",
-                emoji         = "[Emoji]",
-                nvim_lsp      = "[LSP]",
-                path          = "[Path]",
-                cmp_tabnine   = "[T9]",
-                look          = "[Look]",
-                treesitter    = "[TS]",
-                -- ultisnips     = "[US]",
-                luasnip       = "[LuaSnip]",
-                nvim_lua      = "[Lua]",
-                latex_symbols = "[Latex]",
-                })[entry.source.name]
-            return vim_item
-        end,
-        -- format = lspkind.cmp_format({with_text = false, maxwidth = 50})
+      completeopt    = "menu,menuone,noselect",
+      keyword_length = 2,
+      border         = 'rounded',
+      scrollbar      = '║',
+      get_trigger_characters = function(trigger_characters)
+        return vim.tbl_filter(function(char)
+          return char ~= ' '
+        end, trigger_characters)
+      end,
+      autocomplete = {
+        cmp.TriggerEvent.InsertEnter,
+        cmp.TriggerEvent.TextChanged
+      }
     },
 
-    sorting = {
-      comparators = {
-        cmp.config.compare.offset,
-        cmp.config.compare.exact,
-        cmp.config.compare.score,
-
-        require("cmp-under-comparator").under,
-          cmp.config.compare.kind,
-          cmp.config.compare.sort_text,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-      },
-    },
-
-    experimental = {
-        ghost_text  = true,
-    },
-
-    mapping = {
-        ['<C-n>']     = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Insert }),
-        ['<C-p>']     = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Insert }),
-        ['<C-d>']     = cmp.mapping.scroll_docs(-4),
-        ['<C-f>']     = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>']     = cmp.mapping.close(),
-        ['<CR>']      = cmp.mapping.confirm {
-                behavior = cmp.ConfirmBehavior.Select, -- try Insert in lieu of Replace
-                select = true,
-        },
-
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-             cmp.select_next_item({ behavior = types.cmp.SelectBehavior.Select })
-          elseif vim.fn.pumvisible() == 1 then
-            feedkey("<C-n>", "n")
-          elseif vim.fn["vsnip#available"]() == 1 then
-            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-          elseif has_words_before() then
-            cmp.complete()
-          elseif check_back_space() then
-            feedkey("<tab>", "n")
-          else
-            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-          end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-             cmp.select_prev_item({ behavior = types.cmp.SelectBehavior.Select })
-           elseif vim.fn.pumvisible() == 1 then
-            feedkey("<C-p>", "n")
-          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-            feedkey("<Plug>(vsnip-jump-prev)", "")
-          end
-        end, { "i", "s" }),
-   },
-   documentation = {
+    documentation = {
       border       = "rounded",
+      scrollbar    = '║',
       winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
       max_width    = 50,
       min_width    = 50,
       max_height   = math.floor(vim.o.lines * 0.4),
       min_height   = 3,
     },
-    sources = {
-        -- {name = "ultisnips"},
-        {name = 'vsnip'},
-        {name = 'nvim_lsp'},
-        {name = 'orgmode'},
-        {name = 'path'},
-        -- {name = 'cmp_tabnine'},
-        -- {name = 'rg'},
-        {name = 'buffer',
-          opts = {
-            get_bufnrs = function()
-            local bufs = {}
-              for _, win in ipairs(vim.api.nvim_list_wins()) do
-                bufs[vim.api.nvim_win_get_buf(win)] = true
-              end
-              return vim.tbl_keys(bufs)
-              end
-          }
-        },
-        {name = 'treesitter'},
-        {name = 'nvim_lua'},
-        {name = 'calc'},
-        {name = 'look'},
+
+    experimental = {
+      ghost_text  = true,
     },
+  },
+
+  experimental = {
+    ghost_text  = true,
+  },
+
+  formatting = {
+    -- fields = { 'abbr', 'kind', 'menu' },
+    fields = { 'kind', 'abbr'},
+    format = function(entry, vim_item)
+      -- vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
+      vim_item.kind = lspkind.presets.default[vim_item.kind]
+      -- set a name for each source
+      vim_item.menu = ({
+        vsnip         = "⌠VSnip⌡",
+        spell         = "⌠Spell⌡",
+        buffer        = "⌠Buffer⌡",
+        calc          = "⌠Calc⌡",
+        emoji         = "⌠Emoji⌡",
+        nvim_lsp      = "⌠LSP⌡",
+        path          = "⌠Path⌡",
+        cmp_tabnine   = "⌠T9⌡",
+        look          = "⌠Look⌡",
+        treesitter    = "⌠TS⌡",
+        -- ultisnips     = "⌠US⌡",
+        luasnip       = "⌠LuaSnip⌡",
+        nvim_lua      = "⌠Lua⌡",
+        latex_symbols = "⌠Latex⌡",
+      })[entry.source.name]
+      return vim_item
+    end,
+    -- format = lspkind.cmp_format({with_text = false, maxwidth = 50})
+  },
+
+  sorting = {
+    comparators = {
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.score,
+
+      require("cmp-under-comparator").under,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
+
+  mapping = {
+    ['<C-n>']     = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Insert }),
+    ['<C-p>']     = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Insert }),
+    ['<C-d>']     = cmp.mapping.scroll_docs(-4),
+    ['<C-f>']     = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>']     = cmp.mapping.close(),
+    ['<CR>']      = cmp.mapping.confirm {select = false},
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item({ behavior = types.cmp.SelectBehavior.Select })
+      elseif vim.fn.pumvisible() == 1 then
+        feedkey("<C-n>", "n")
+      elseif vim.fn["vsnip#available"]() == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      elseif check_back_space() then
+        feedkey("<tab>", "n")
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({ behavior = types.cmp.SelectBehavior.Select })
+      elseif vim.fn.pumvisible() == 1 then
+        feedkey("<C-p>", "n")
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+  },
+  sources = {
+    -- {name = "ultisnips"},
+    {name = 'vsnip'},
+    {name = 'nvim_lsp'},
+    {name = 'orgmode'},
+    {name = 'vim-dadbod-completion'},
+    {name = 'path'},
+    -- {name = 'cmp_tabnine'},
+    -- {name = 'rg'},
+    {name = 'buffer',
+      opts = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
+        end
+      }
+    },
+    {name = 'treesitter'},
+    {name = 'nvim_lua'},
+    {name = 'calc'},
+    {name = 'look'},
+  },
 })
 
 -- Autopairs
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr       = true,
-  map_complete = true,
-  auto_select  = true
-})
+-- require("nvim-autopairs.completion.cmp").setup({
+--   map_cr       = true,
+--   map_complete = true,
+--   auto_select  = true
+-- })
 
 -- End of File
 
