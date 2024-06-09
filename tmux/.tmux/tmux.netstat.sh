e94c0d68cf90779a0235e049164eb474da44e4f5
Ratheesh S <ratheeshreddy@gmail.com>
2024-06-09 16:50:12 +0530

    tmux: change online glyph character

--- tmux/.tmux/tmux.netstat.sh
+++ tmux/.tmux/tmux.netstat.sh
@@ -4,9 +4,9 @@
 
 nc -z -w 1 8.8.8.8 53 &>/dev/null
 if [ $? -ne 0 ];then
-    echo -e "#[fg=colour160]◍ #[default]"
+    echo -e "#[fg=colour160]🔴#[default]"
 else
-    echo -e "#[fg=colour33]◍ #[default]"
+    echo -e "#[fg=colour33]🔴#[default]"
 fi
 
 # End of File
