b3df214ea930c814679d474f117b10a95a0c3caf
Ratheesh S <ratheeshreddy@gmail.com>
2025-04-11 08:45:05 +0530

    tmux: fix online glyph in netstat script

--- tmux/.tmux/tmux.netstat.sh
+++ tmux/.tmux/tmux.netstat.sh
@@ -6,7 +6,7 @@ nc -z -w 1 8.8.8.8 53 &>/dev/null
 if [ $? -ne 0 ];then
     echo -e "#[fg=colour160]🔴#[default]"
 else
-    echo -e "#[fg=colour33]🔴#[default]"
+    echo -e "#[fg=colour33]🔵#[default]"
 fi
 
 # End of File
