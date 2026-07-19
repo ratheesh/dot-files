25bbc74bcc28d81545a9e500be9488e7dee539eb
Ratheesh <ratheeshreddy@gmail.com>
2026-07-19 14:00:38 +0530

    tmux:cpu: fix cpu utilization issue for MacOS add icon to segment

--- tmux/.tmux/tmux.cpu.sh
+++ tmux/.tmux/tmux.cpu.sh
@@ -3,9 +3,11 @@
 STATE="${TMPDIR:-/tmp}/tmux-cpu-${USER:-$UID}.state"
 
 if [[ "$(uname)" == "Darwin" ]]; then
-  RAW=$(top -l 2 -n 0 -s 0 2>/dev/null | tail -1 | awk '{print $3}')
+  RAW=$(top -l 2 -n 0 -s 0 | head -4 | tail -1 | awk '{print $3}')
   RAW="${RAW//%/}"
-  echo "$RAW%"
+  # echo " $RAW%"
+  printf -v cpu "%04.1f%" "$RAW"
+  echo "#[fg=#B465F1] #[fg=#c0c0c0]$cpu%"
   exit 0
 fi
 
@@ -22,8 +24,8 @@ printf '%s %s\n' "$IDLE" "$TOTAL" > "$STATE"
 DTOTAL=$((TOTAL - PTOTAL))
 DIDLE=$((IDLE - PIDLE))
 if [ "$DTOTAL" -le 0 ]; then
-  echo "0.0%"
+  echo "00.0%"
 else
-  awk -v dt="$DTOTAL" -v di="$DIDLE" 'BEGIN { printf "%.1f%%", 100 * (dt - di) / dt }'
+  awk -v dt="$DTOTAL" -v di="$DIDLE" 'BEGIN { printf "#[fg=#B465F1] #[fg=#c0c0c0]%04.1f%%", 100 * (dt - di) / dt }'
 fi
 # End of File
